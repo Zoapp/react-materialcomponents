381ce0143297f8eb15d3b3ed44fa561f0906a135
mikbry <mik@opla.ai>
2018-02-20 22:20:06 +0100

    Fix ListItem onClick

--- src/libs/list/item.jsx
+++ src/libs/list/item.jsx
@@ -19,7 +19,7 @@ import Rmdc from "../";
 const MDC_LISTITEM = "mdc-list-item";
 
 const ListItem = ({
-  children, type, icon, activated, imgSrc, imgSize, imgLabel, secondaryText, href, ...props
+  children, type, icon, activated, imgSrc, imgSize, imgLabel, secondaryText, href, onClick, ...props
 }) => {
   let classes = MDC_LISTITEM;
   let graphic;
@@ -42,10 +42,19 @@ const ListItem = ({
   if (secondaryText) {
     text = (<span className="mdc-list-item__text">{text}<span className="mdc-list-item__secondary-text">{secondaryText}</span></span>);
   }
-  if (type === "a") {
-    return Rmdc.render(<a className={classes} href={href} >{graphic}{text}{meta}</a>, props);
+  const p = {};
+  if (onClick) {
+    p.onCLick = onClick;
   }
-  return Rmdc.render(<li className={classes} >{graphic}{text}{meta}</li>, props);
+  let el;
+  if (type === "a" || href) {
+    el = <a className={classes} href={href} {...p} >{graphic}{text}{meta}</a>;
+  } else {
+    p.role = "menuitem";
+    p.onKeyPress = () => {};
+    el = <li className={classes} {...p} >{graphic}{text}{meta}</li>;
+  }
+  return Rmdc.render(el, props);
 };
 
 ListItem.defaultProps = {
@@ -59,6 +68,7 @@ ListItem.defaultProps = {
   imgLabel: null,
   secondaryText: null,
   href: null,
+  onClick: null,
 };
 
 ListItem.propTypes = {
@@ -72,6 +82,7 @@ ListItem.propTypes = {
   imgLabel: PropTypes.string,
   secondaryText: PropTypes.string,
   href: PropTypes.string,
+  onClick: PropTypes.func,
 };
 
 export default ListItem;
