93fdb4243345438061d71d84a2cd2e4a922bd297
williamdaconceicao <willidaconceicao@gmail.com>
2018-02-23 11:19:45 +0100

    Interresting bug

--- src/demos/components/properties.jsx
+++ src/demos/components/properties.jsx
@@ -4,35 +4,36 @@ import { Grid, Inner, Cell } from "../../../src/libs";
 
 const Head = ["Name", "Type", "Desc"];
 
-const Property = Component => (
-  <Grid>
-    <Inner>
-      {Head.map(title => (
-        <Cell key={title}>{title}</Cell>
-      ))}
-    </Inner>
-    {Object.keys(PropertyJson).forEach((props) => {
-      if (props === Component.name) {
-        if (PropertyJson[props].length === undefined) {
-          Object.keys(PropertyJson[props]).forEach((GeneralPropertyName) => {
-            PropertyJson[props][GeneralPropertyName].forEach(element =>
+function Property(Component) {
+  return (
+    <Grid>
+      <Inner>
+        {Head.map(title => <Cell key={title}>{title}</Cell>)}
+      </Inner>
+      {Object.keys(PropertyJson).forEach(props => {
+        if (props === Component.name) {
+          if (PropertyJson[props].length === undefined) {
+            Object.keys(PropertyJson[props]).forEach((GeneralPropertyName) => {
+              PropertyJson[props][GeneralPropertyName].forEach(element =>
+                (
+                  <Inner style={{ paddingBottom: "16px" }}>
+                    {element.forEach(value => <Cell> {value} </Cell>)}
+                  </Inner>
+                ));
+            });
+          } else {
+            PropertyJson[props].forEach(element =>
               (
                 <Inner style={{ paddingBottom: "16px" }}>
-                  {element.forEach(PropertyType => <Cell> {PropertyType} </Cell>)}
+                  {element.map(value => <Cell key={value}>{value}</Cell>)}
                 </Inner>
               ));
-          });
-        } else {
-          PropertyJson[props].forEach(element =>
-            (
-              <Inner style={{ paddingBottom: "16px" }}>
-                {element.forEach(PropertyType => <Cell> {PropertyType} </Cell>)}
-              </Inner>
-            ));
+          }
         }
-      }
-    })}
-  </Grid>
-);
+      })}
+      <div>Salut</div>
+    </Grid>
+  )
+};
 
 export default Property;
