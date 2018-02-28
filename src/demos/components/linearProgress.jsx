8f2375798155f96828cae005988301334c94b4dd
williamdaconceicao <willidaconceicao@gmail.com>
2018-02-28 13:19:29 +0100

    Create a Property component and his test

--- src/demos/components/linearProgress.jsx
+++ src/demos/components/linearProgress.jsx
@@ -5,40 +5,14 @@
  * LICENSE file in the root directory of this source tree.
  */
 import React from "react";
-import { LinearProgress, Grid, Inner, Cell } from "../../../src/libs";
-
-const head = [
-  "Name", "Type", "Desc",
-];
-
-const LinearProperties = [
-  ["color", "string", "The color of the progress bar,  defaults to primary color of theme."],
-  ["max", "number", "The max value of progress,  only works in determinate mode."],
-  ["min", "number", "The min value of progress,  only works in determinate mode."],
-  ["mode", "enum:'determinate', 'indeterminate', 'indeterminate'", "The mode of show your progress,  indeterminate for when there is no value for progress."],
-  ["style", "object", "Override the inline-styles of the root element."],
-  ["value", "number", "The value of progress, only works in determinate mode."],
-];
+import { LinearProgress } from "../../../src/libs";
+import Property from "./property";
 
 export default () => (
   <section>
     <h1>LinearProgress examples </h1>
     <LinearProgress progress={0.2} buffer={0.5} />
     <h2>Linear Properties</h2>
-    <Grid>
-      <Inner>
-        {head.map(title => (
-          <Cell key={title}>{title}</Cell>
-        ))}
-      </Inner>
-      {LinearProperties.map((row, idx) => (
-        // eslint-disable-next-line
-        <Inner key={idx} style={{ paddingBottom: "16px" }}>
-          {row.map(col => (
-            <Cell key={col}> {col} </Cell>
-          ))}
-        </Inner>
-      ))}
-    </Grid>
+    <Property name="Linear" />
   </section>
 );
