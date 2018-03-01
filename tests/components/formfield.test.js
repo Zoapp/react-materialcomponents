47ebbb8501e77ef1555543c704c6bf23172db2c5
williamdaconceicao <willidaconceicao@gmail.com>
2018-03-01 17:57:18 +0100

    Formfield test creation

---
+++ tests/components/formfield.test.js
@@ -0,0 +1,14 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import FormField from "@libs/components/formField";
+
+describe("components/FormField", () => {
+  it("can have children", () => {
+    const tree = renderer.create(
+      <FormField>
+        Children is here!
+      </FormField>
+    ).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
